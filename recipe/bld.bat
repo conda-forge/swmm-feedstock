d5b15652480182c91f61afefe7f0c7c3db7316a6
Filipe Fernandes <ocefpaf@gmail.com>
2023-06-07 10:31:34 -0300

    fix directory

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,12 @@
+mkdir %SRC_DIR%\build
+cd %SRC_DIR%\build
+
 set "CMAKE_GENERATOR=NMake Makefiles"
-cmake.exe -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX CMakeLists.txt
+cmake.exe -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX CMakeLists.txt %SRC_DIR%
 if errorlevel 1 exit /b 1
-cmake.exe --build . --config Release
+
+cmake.exe --build %SRC_DIR% --config Release
 if errorlevel 1 exit /b 1
+
 if not exist "%PREFIX%\" mkdir "%PREFIX%"
 COPY bin\* %PREFIX%\
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,12 @@
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX CMakeLists.txt
-cmake --build . --config Release
+mkdir build && cd build
+
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      CMakeLists.txt \
+      ${SRC_DIR}
+
+cmake --build ${SRC_DIR} --config Release
 # cmake --install $SRC_DIR -v
-mkdir -p $PREFIX/bin $PREFIX/lib
-install $SRC_DIR/bin/run-swmm $PREFIX/bin
-install $SRC_DIR/lib/* $PREFIX/lib
+mkdir -p ${PREFIX}/bin ${PREFIX}/lib
+install ${SRC_DIR}/bin/run-swmm ${PREFIX}/bin
+install ${SRC_DIR}/lib/* ${PREFIX}/lib
