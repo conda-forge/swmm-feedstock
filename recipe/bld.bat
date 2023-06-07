507a9c499913b52da4e9408863b1663edfb48b7b
Filipe Fernandes <ocefpaf@gmail.com>
2023-06-07 12:38:57 -0300

    try ninja

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,15 @@
 mkdir %SRC_DIR%\build
 cd %SRC_DIR%\build
 
-set "CMAKE_GENERATOR=NMake Makefiles"
+set "CMAKE_GENERATOR=Ninja"
 cmake -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
       CMakeLists.txt ^
       %SRC_DIR%
 if errorlevel 1 exit /b 1
 
-cmake --build . --config %BUILD_TYPE% --target install
-if errorlevel 1 exit /b 1
+ninja
+if errorlevel 1 exit 1
+
+@REM cmake --build . --config %BUILD_TYPE% --target install
+@REM if errorlevel 1 exit /b 1
